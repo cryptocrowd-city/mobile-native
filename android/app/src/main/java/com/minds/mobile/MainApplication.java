bc618baf2df2709c357a71f79db2e2cf6b3d1ac0
Martin Santangelo <msantang78@gmail.com>
2019-09-04 19:19:43 -0300

    (chore) replace qrcode package - vector icons and menu packages updated

--- android/app/src/main/java/com/minds/mobile/MainApplication.java
+++ android/app/src/main/java/com/minds/mobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.minds.mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import io.sentry.RNSentryPackage;
 import com.reactnativejitsimeet.JitsiMeetPackage;
@@ -58,6 +59,7 @@ public class MainApplication extends Application implements ShareApplication, Re
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new RNDeviceInfo(),
             new RNSentryPackage(),
             new JitsiMeetPackage(),
