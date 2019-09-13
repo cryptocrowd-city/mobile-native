cb09cb54829428356306fae0cd32fb0d305ede2a
Martin Santangelo <msantang78@gmail.com>
2019-09-13 14:23:13 -0300

    (feat) add position dots to tiers carousel

--- src/wire/tiers/SubscriptionTierCarousel.js
+++ src/wire/tiers/SubscriptionTierCarousel.js
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react';
-import { Text, View } from 'react-native';
-import Carousel from 'react-native-snap-carousel';
+import { Text, View, StyleSheet } from 'react-native';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
 
 import featuresService from '../../common/services/features.service';
 import { CommonStyle as CS } from '../../styles/Common';
 import viewportPercentage from '../../common/helpers/viewportPercentage';
+import i18n from '../../common/services/i18n.service';
 
 const {value: slideWidth, viewportHeight} = viewportPercentage(75);
 const {value: itemHorizontalMargin} = viewportPercentage(2);
@@ -85,7 +86,7 @@ export default class SubscriptionTierCarousel extends PureComponent {
     return (
       <View key={`rewards${row.item.amount}`} style={[CS.rowJustifyCenter, CS.backgroundLightGreyed, CS.borderRadius5x, CS.padding2x, CS.border, CS.borderGreyed]}>
         <View style={CS.columnAlignCenter}>
-          <Text style={[CS.fontXXL, CS.fontMedium, CS.colorDark]}>{amount} {this.getPluralizedCurrency(currency, row.item.amount)} / month</Text>
+          <Text style={[CS.fontXXL, CS.fontMedium, CS.colorDark]}>{i18n.t('wire.amountMonth',{amount: amount + ' ' + this.getPluralizedCurrency(currency, row.item.amount)})}</Text>
           <Text numberOfLines={5} style={[CS.fontL, CS.fontHairline, CS.colorDark]}>{row.item.description}</Text>
         </View>
       </View>
@@ -109,20 +110,48 @@ export default class SubscriptionTierCarousel extends PureComponent {
     let current = this.rewards.findIndex(r => r.amount == this.props.amount && r.currency == this.props.currency);
 
     return (
-      <Carousel
-        onSnapToItem={this.onSelected}
-        enableSnap={true}
-        // layout={'tinder'}
-        layoutCardOffset={`10`}
-        ref={(c) => { this._carousel = c; }}
-        data={this.rewards}
-        firstItem={current}
-        renderItem={this._renderItem}
-        inactiveSlideScale={0.94}
-        inactiveSlideOpacity={0}
-        sliderWidth={viewportHeight}
-        itemWidth={itemWidth}
-      />
+      <View style={[]}>
+        <Pagination
+          dotsLength={this.rewards.length}
+          activeDotIndex={current === -1 ? 0 : current}
+          containerStyle={styles.paginatorContainer}
+          dotStyle={styles.dot}
+          inactiveDotOpacity={0.4}
+          inactiveDotScale={0.6}
+        />
+        <Carousel
+          onSnapToItem={this.onSelected}
+          containerCustomStyle={styles.carousel}
+          enableSnap={true}
+          // layout={'tinder'}
+          layoutCardOffset={`10`}
+          ref={(c) => { this._carousel = c; }}
+          data={this.rewards}
+          firstItem={current}
+          renderItem={this._renderItem}
+          inactiveSlideScale={0.94}
+          inactiveSlideOpacity={0}
+          sliderWidth={viewportHeight}
+          itemWidth={itemWidth}
+        />
+
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  carousel: {
+    flexGrow: 0,
+  },
+  paginatorContainer: {
+    paddingVertical: 10
+  },
+  dot: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    marginHorizontal: 0,
+    backgroundColor: 'rgba(46, 46, 46, 0.92)'
+  }
+})
