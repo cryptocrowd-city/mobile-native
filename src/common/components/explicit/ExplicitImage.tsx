5afd613f58a93be7cc78794b745e7d79df0bd7a3
Martin Santangelo <msantang78@gmail.com>
2020-05-07 17:25:56 -0300

    (feat) implement TS on minds video component

--- src/common/components/explicit/ExplicitImage.tsx
+++ src/common/components/explicit/ExplicitImage.tsx
@@ -1,21 +1,30 @@
 //@ts-nocheck
 import React, { Component } from 'react';
-
 import { observer } from 'mobx-react';
-
-import { findNodeHandle, Platform, Image, View } from 'react-native';
+import { View } from 'react-native';
 
 import FastImage from 'react-native-fast-image';
+import type { Source } from 'react-native-fast-image';
 import { createImageProgress } from 'react-native-image-progress';
 import ProgressCircle from 'react-native-progress/Circle';
-import { Icon } from 'react-native-elements';
+
 import { CommonStyle } from '../../../styles/Common';
+import type BaseModel from '../../BaseModel';
 
 const ProgressFastImage = createImageProgress(FastImage);
-const ProgressImage = createImageProgress(Image);
+
+type PropsType = {
+  onLoadEnd: () => void;
+  source: Source;
+  onError: (error: any) => void;
+  entity?: BaseModel;
+};
 
 @observer
-export default class ExplicitImage extends Component {
+export default class ExplicitImage extends Component<
+  PropsType,
+  { ready: boolean }
+> {
   state = {
     ready: false,
   };
@@ -54,8 +63,9 @@ export default class ExplicitImage extends Component {
       !this.props.source ||
       !this.props.source.uri ||
       this.props.source.uri.indexOf('//') < 0
-    )
-      return <View></View>;
+    ) {
+      return <View />;
+    }
 
     switch (loadingIndicator) {
       case undefined:
